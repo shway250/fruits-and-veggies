19f06f7176827e5a70cfb1d6627babb438caa83b
shway250 <jackson_duhon@yahoo.com>
2016-09-16 09:01:18 -0700

    GAH, MY EYES

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 /* setup your angular app here */
+var introApp = angular.module('IntroApp', []);
 
 // debug stuff to show the app is loading and fruit / veggies are available
 console.log('App Started');
 console.log('Fruit count', fruits.length);
 console.log('Veggie count', vegetables.length);
+
+
+
+introApp.controller('listCtrl', ['$scope', function($scope){
+  
+  $scope.toDoList = fruits.concat(vegetables);
+
+  $scope.toggleFruit = function() {
+        
+  }
+
+}]);
